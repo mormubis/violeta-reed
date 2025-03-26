3ac814edd96d6f170d60440f84b3a255f070e187
Adrian de la Rosa <adelarosabretin@coursera.org>
2025-03-26 19:05:12 -0400

    fix: robots.txt

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,5 @@ import type { RouteConfig } from '@react-router/dev/routes';
 export default [
   index('routes/home.tsx'),
   route('libros', 'routes/books.tsx'),
+  route('robots.txt', 'routes/robots[.]txt.tsx'),
 ] satisfies RouteConfig;
